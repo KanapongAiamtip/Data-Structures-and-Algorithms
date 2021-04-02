dd7b88c4bf7739073a5c7fd48e044a76775eacbd
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-04-02 23:09:30 +0700

    Create Lab1Task2.java

---
+++ Lab 1/Lab1Task2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Lab1Task2
+{
+  public static void main(String[] args)
+  {
+    Scanner kb = new Scanner(System.in);
+    System.out.print("Insert Numbers : ");
+    int s = kb.nextInt();
+    System.out.println("num = "+s);
+  }
+}
