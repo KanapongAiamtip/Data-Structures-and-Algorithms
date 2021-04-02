26fa370e806f5d1c194b8edbcf1f1c22aa965c3d
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-04-03 02:16:51 +0700

    Update Lab2Task3.java

--- Lab 2/Lab2Task3.java
+++ Lab 2/Lab2Task3.java
@@ -1,21 +1,20 @@
-//import java.util.*;
 import java.util.Scanner;
 public class Lab2Task3
 {
     public static void main(String[] args)
-		{
+    {
  	    ArrayCollection mydata = new ArrayCollection(10);
- 	 	  Object d = input();
- 		   mydata.add(d);
-		   for(int i=1;i<=4;i++)
-		   {
+ 	    Object d = input();
+   	    mydata.add(d);
+	    for(int i=1;i<=4;i++)
+	    {
 	        mydata.add(input());
-	     }
- 		    System.out.println(mydata.size());
+	    }
+ 	 System.out.println(mydata.size());
      }
 
      private static Object input()
-		 {
+     {
        Scanner in = new Scanner(System.in);
        String s = in.nextLine();
        return s;
