6a5859dc322726e415aec322b1abf43c10175826
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-04-03 02:20:03 +0700

    Update Lab2Task2.java

--- Lab 2/Lab2Task2.java
+++ Lab 2/Lab2Task2.java
@@ -1,8 +1,8 @@
 public class Lab2Task2
 {
-		public static void main(String[] args)
-		{
+	public static void main(String[] args)
+	{
  		ArrayCollection mydata = new ArrayCollection(10);
  		System.out.println(mydata.size());
-		}
+	}
 }
