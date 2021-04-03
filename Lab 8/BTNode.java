b680a45dac931f15fe46fe21fe600a09a3bf60e3
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-04-03 14:51:26 +0700

    Create BTNode.java

---
+++ Lab 8/BTNode.java
@@ -0,0 +1,16 @@
+public class BTNode
+{
+  public BTNode left, right; public int data;
+  public BTNode()
+  {
+    left = null;
+    right = null;
+    data = 0;// constructor
+  }
+  public BTNode(int data)
+  { // constructor
+    left = null;
+    right = null;
+    this.data = data;
+  }
+}
