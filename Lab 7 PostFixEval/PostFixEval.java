c1315d3ffad54128711bf86a2bd38fd980f5d2c8
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-04-03 14:44:35 +0700

    Update PostFixEval.java

--- Lab 7 PostFixEval/PostFixEval.java
+++ Lab 7 PostFixEval/PostFixEval.java
@@ -10,37 +10,37 @@ public class PostFixEval
 
     public static int postfixEvaluate(String exp)
     {
-	 	 Stack<Integer> s = new Stack<Integer>();
-		 Scanner tokens = new Scanner(exp);
-		 while (tokens.hasNext())
+	 Stack<Integer> s = new Stack<Integer>();
+	 Scanner tokens = new Scanner(exp);
+     	 while (tokens.hasNext())
          {
-			if (tokens.hasNextInt())
-         	{
-				s.push(tokens.nextInt());
+		if (tokens.hasNextInt())
+		{
+		   s.push(tokens.nextInt());
+	   	}
+		else   //  "+", "-", "*", "/"
+	 	{
+			int num2 = s.pop();
+			int num1 = s.pop();
+			String op = tokens.next();
+			if (op.equals("+"))
+			{
+			   s.push(num1 + num2);
 			}
-           else   //  "+", "-", "*", "/"
-           {
-				int num2 = s.pop();
-				int num1 = s.pop();
-				String op = tokens.next();
-				if (op.equals("+"))
-             	{
-					s.push(num1 + num2);
-				}
-             	else if (op.equals("-"))
-             	{
-					s.push(num1 - num2);
-				}
-             	else if (op.equals("*"))
-             	{
-				    s.push(num1 * num2);
-				}
-             	else
-             	{
-					s.push(num1 / num2);
-				}
+			else if (op.equals("-"))
+			{
+			   s.push(num1 - num2);
 			}
-		}
-		return s.pop();
+			else if (op.equals("*"))
+			{
+		           s.push(num1 * num2);
+			}
+			else
+			{
+			   s.push(num1 / num2);
+			}
+		 }
+	}
+	   return s.pop();
   }
 }
