b1d8cafbd5a9d9a88f213b6a4db0fa840229c2ce
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-04-03 14:34:38 +0700

    Create PostFixEval.java

---
+++ Lab 7 PostFixEval/PostFixEval.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class PostFixEval
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String x = in.nextLine();
+        System.out.println(postfixEvaluate(x));
+    }
+
+    public static int postfixEvaluate(String exp)
+    {
+	 	 Stack<Integer> s = new Stack<Integer>();
+		 Scanner tokens = new Scanner(exp);
+		 while (tokens.hasNext())
+         {
+			if (tokens.hasNextInt())
+         	{
+				s.push(tokens.nextInt());
+			}
+           else   //  "+", "-", "*", "/"
+           {
+				int num2 = s.pop();
+				int num1 = s.pop();
+				String op = tokens.next();
+				if (op.equals("+"))
+             	{
+					s.push(num1 + num2);
+				}
+             	else if (op.equals("-"))
+             	{
+					s.push(num1 - num2);
+				}
+             	else if (op.equals("*"))
+             	{
+				    s.push(num1 * num2);
+				}
+             	else
+             	{
+					s.push(num1 / num2);
+				}
+			}
+		}
+		return s.pop();
+  }
+}
